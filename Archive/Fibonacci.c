db72bfa365cd04b7ec1125134672828fc043e9cf
ThinkerWel <92936147+ThinkerWel@users.noreply.github.com>
2022-05-16 20:11:53 +0300

    Create Fibonacci.c

---
+++ Archive/Fibonacci.c
@@ -0,0 +1,32 @@
+ #include <stdio.h>
+#include <stdlib.h>
+
+int funcFub(int N);
+
+int main(void)
+{
+   int N;
+
+   printf(" N = ");
+   scanf("%d",&N);
+   for(int i=1;i<=N;i++)
+
+   printf(" %d ",funcFub(i));
+   return 0;
+}
+
+int funcFub(int N)
+{
+    if(N==1 || N==2)
+        return 1;
+    return funcFub(N-1)+funcFub(N   -2);
+
+}
+
+//
+long factorial(long n)
+{
+	if (n==0 || n==1)
+		return 1;
+	return factorial(n - 1) * n;
+}
